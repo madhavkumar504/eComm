bc9d047d3e57a4f7cd3a72a90f0eba0398d438b7
madhav_karmahealthcare <madhav_karmahealthcare.in>
2021-12-17 23:38:12 +0530

    Login done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="container-fluid">
+    <div class="panel panel-primary">
+    <div class="panel-heading">Panel Heading</div>
+    <div class="panel-body">Panel Content</div>
+    </div>
+</div>
